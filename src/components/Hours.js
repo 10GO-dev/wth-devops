b6e37a5182945b0b907b83eefdc187d2a2c28646
10GO-dev <60454093+10GO-dev@users.noreply.github.com>
2024-08-28 19:52:47 +0000

    Fix hours component

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,42 +1,23 @@
 const Hours = () => {
-    const Hours = () => {
         const today = new Date().getDay();
-        let hoursLeft;
+        let todayHours = "";
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
         if (today >= 1 && today <= 5) {
             // Monday to Friday
-            const currentHour = new Date().getHours();
-            const closingHour = 16;
+            todayHours = "10:00 - 16:00";
 
-            if (currentHour < 10) {
-                hoursLeft = closingHour - currentHour;
-            } else if (currentHour >= 10 && currentHour < closingHour) {
-                hoursLeft = closingHour - currentHour;
-            } else {
-                hoursLeft = 0;
-            }
         } else if (today === 0 || today === 6) {
-            // Saturday and Sunday
-            const currentHour = new Date().getHours();
-            const closingHour = 20;
-
-            if (currentHour < 9) {
-                hoursLeft = closingHour - currentHour;
-            } else if (currentHour >= 9 && currentHour < closingHour) {
-                hoursLeft = closingHour - currentHour;
-            } else {
-                hoursLeft = 0;
-            }
-        } else {
-            hoursLeft = 0;
+            // Sunday or Saturday
+            todayHours = "9:00 - 20:00";
         }
 
         return (
             <div>
-                <p>Today's hours left: {hoursLeft}</p>
+                <h2>Today's Hours</h2>
+                <p>{days[today] } {todayHours} </p>
             </div>
         );
-    }
 }
 
 export default Hours;
